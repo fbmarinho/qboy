21d5301ce3e75c36c2aa476fa8016be30a5707b4
Felipr <felipegigamail@gmail.com>
2019-08-13 17:57:32 -0300

    Mudancas iniciais

--- Qboy.cpp
+++ Qboy.cpp
@@ -19,7 +19,10 @@
 #include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 
-// 1      2      3      4      5      6         7     8  
+//Time controle
+#include <Ticker.h>
+Ticker pinger;
+// 1      2      3      4      5      6         7     8
 // VCC / GND  / SCE  / RST /  DC / DN(MOSI) / SCLK / LED
 // pins
 const int8_t RST_PIN = D2;
@@ -32,97 +35,118 @@ const int8_t BL_PIN = D0;
 Adafruit_PCD8544 screen = Adafruit_PCD8544(DC_PIN, CE_PIN, RST_PIN);
 
 //Constructor
-Qboy::Qboy(const char* id)
+Qboy::Qboy(const char *id)
 {
-    //setup
-    pinMode(BL_PIN, OUTPUT);
-    _id = id;
-    showLogo();
+  //setup
+
+  pinMode(BL_PIN, OUTPUT);
+  _id = id;
+  showLogo();
 }
 
 //Conectar a rede
-void Qboy::connect(const char* ssid, const char* password)
+void Qboy::connect(const char *ssid, const char *password, int checkInterval)
 {
-  WiFi.begin(ssid,password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    screen.drawLine(40,83,45,83, 0x0000);
-    delay(200);
-    screen.display();
-    screen.drawLine(43,80,43,85, 0x0000);
-    delay(200);
-    screen.display();
-  }
-
-  screen.clearDisplay();
-  screen.setCursor(17,20);
-  screen.print("CONECTADO");
-  screen.display();
-  delay(2000);
-
-  pair();
-  
+  isConnected = false;
+  WiFi.begin(ssid, password);
+  pinger.attach(checkInterval, &Qboy::_ping, this);
 }
 
+void Qboy::_ping(Qboy *pThis)
+{
+  pThis->ping();
+}
+
+void Qboy::ping()
+{
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    Serial.println("Ping !");
+    isConnected = true;
+    pair();
+  }
+}
 
 //Parear dispositivo
 void Qboy::pair()
 {
-    std::string url = std::string("easystop.com.br/api/qboy/dispositivo/parear/") + std::string(_id);
-    
-    HTTPClient http;  //Object of class HTTPClient
+  Serial.print("Chamando: ");
 
-    http.begin(url.c_str());
+  std::string url = std::string("http://easystop.com.br/api/qboy/dispositivo/parear/") + std::string(_id);
 
-    screen.clearDisplay();
-    screen.setCursor(0,0);
-    screen.print(url.c_str());
-    screen.display();
+  Serial.println(url.c_str());
 
+  HTTPClient http;
 
-    int httpCode = http.GET();
-    //Check the returning code                                                                  
-    if (httpCode > 0) {
+  http.begin(url.c_str());
 
-        screen.clearDisplay();
-        screen.setCursor(20,20);
-        screen.print(httpCode);
-        screen.display();
-        vibrate(2,1000);
+  int httpCode = http.GET();
 
-    } else {
+  //Check the returning code
+  if (httpCode > 0)
+  {
+    // Parsing
+    const size_t capacity = JSON_ARRAY_SIZE(1) + JSON_OBJECT_SIZE(17) + 360;
+    DynamicJsonDocument doc(capacity);
 
-      backlightOff();
+    String json = http.getString();
 
+    if (json != "[]")
+    {
+
+      deserializeJson(doc, json);
+
+      JsonObject obj = doc[0];
+
+      idPedido = obj["id_pedido_exame"];
+
+      Serial.print("Pedido: ");
+      Serial.println(idPedido);
+
+      isPaired = true;
+      //const char *obj_id_Chamado = obj["id_Chamado"];
+      //const char *obj_id_pedido_exame = obj["id_pedido_exame"];
+      // const char *obj_id_dispositivo = obj["id_dispositivo"];
+      // const char *obj_dt_associacao = obj["dt_associacao"];
+      // const char *obj_is_Chamando = obj["is_Chamando"];
+      // const char *obj_mensagem = obj["mensagem"];
+      // const char *obj_segundos_mensagem = obj["segundos_mensagem"];
+      // const char *obj_qtd_repeticoes_mensagem = obj["qtd_repeticoes_mensagem"];
+      // const char *obj_mensagem1 = obj["mensagem1"];
+      // const char *obj_segundos_mensagem1 = obj["segundos_mensagem1"];
+      // const char *obj_qtd_repeticoes_mensagem1 = obj["qtd_repeticoes_mensagem1"];
+      // const char *obj_qtd_vibracoes = obj["qtd_vibracoes"];
+      // const char *obj_segundos_vibracoes = obj["segundos_vibracoes"];
     }
-     
-    http.end();   //Close connection
+  }
+  else
+  {
+    Serial.println("Erro ao parear !");
+  }
 
+  http.end(); //Close connection
 }
 
-
-//Escutar chamados
-void Qboy::listen()
-{
-    
-    
-}
-
+// //Escutar chamados
+// void Qboy::listen()
+// {
+// }
 
 // Private methods
 void Qboy::backlightOn()
 {
-    digitalWrite(BL_PIN, HIGH);
+  digitalWrite(BL_PIN, HIGH);
 }
 
 void Qboy::backlightOff()
 {
-    digitalWrite(BL_PIN, LOW);
+  digitalWrite(BL_PIN, LOW);
 }
 
 void Qboy::vibrate(int n, int interval)
 {
-  for(int i = 0; i<= n; i++){
+  for (int i = 0; i <= n; i++)
+  {
     digitalWrite(D8, HIGH);
     delay(interval);
     digitalWrite(D8, LOW);
@@ -137,7 +161,7 @@ void Qboy::showLogo()
   screen.setTextSize(1);
   screen.setTextColor(BLACK);
   screen.clearDisplay();
-  screen.setCursor(0,0);
-  screen.drawBitmap(0, 0,  logo, 84, 48, BLACK);
+  screen.setCursor(0, 0);
+  screen.drawBitmap(0, 0, logo, 84, 48, BLACK);
   screen.display();
 }
--- Qboy.h
+++ Qboy.h
@@ -11,18 +11,24 @@
 
 class Qboy
 {
-  public:
-    Qboy(const char* id);
-    void connect(const char* ssid, const char* password);
-    void pair();
-    void listen();
-    bool isConnected;
-  private:
-      const char* _id;
-      void showLogo();
-      void backlightOn();
-      void backlightOff();
-      void vibrate(int n, int interval);
+public:
+  Qboy(const char *id);
+  void connect(const char *ssid, const char *password, int checkInterval);
+
+  int idPedido;
+  bool isConnected;
+  bool isPaired;
+
+private:
+  const char *_id;
+  static void _ping(Qboy *pThis);
+  void ping();
+  void pair();
+  void setConnectionState(bool state);
+  void showLogo();
+  void backlightOn();
+  void backlightOff();
+  void vibrate(int n, int interval);
 };
 
 #endif
